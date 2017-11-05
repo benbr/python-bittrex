12eaad8cca23a09dd052f7129928b54a505db26e
Ubuntu <ubuntu@ip-172-31-21-118.eu-west-1.compute.internal>
2017-11-05 22:16:38 +0000

    added getticks endpoint

--- bittrex/bittrex.py
+++ bittrex/bittrex.py
@@ -229,6 +229,15 @@ class Bittrex(object):
             API_V1_1: '/public/getticker',
         }, options={'market': market}, protection=PROTECTION_PUB)
 
+    def get_ticks(self):
+        """
+	Used to get ticker history. available >2.0
+	"""
+	return self._api_query(path_dict={
+	    API_V1_1: '/pub/Market/GetTicks',
+            API_V2_0: '/pub/Market/GetTicks'
+        }, protection=PROTECTION_PUB)
+
     def get_market_summaries(self):
         """
         Used to get the last 24 hour summary of all active exchanges
